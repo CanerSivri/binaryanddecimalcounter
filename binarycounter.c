bc03d05596723a2445b9cba1173781b1a9f3cc97
Caner Sivri <canersivri@protonmail.com>
2024-06-30 16:39:44 +0300

    Add files via upload

---
+++ binarycounter.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int countOnes(int num){
+    int count=0;
+    while(num > 0){
+        if(num % 2 == 1){
+            count++;
+        }
+        num /= 2;
+    }
+    return count;
+}
+
+void printStars(int count){
+    for(int i=0;i<count;i++){
+        printf("*");
+    }
+}
+
+int main(){
+    int number;
+
+    printf("Please enter an integer number: ");
+    scanf("%d",&number);
+
+    int result = countOnes(number);
+    printStars(result);
+    return 0;
+}
