c6b0b7a27e040207d9636184f473a9f6cbef0423
Caner Sivri <canersivri@protonmail.com>
2024-06-30 17:19:51 +0300

    Add files via upload

---
+++ decimaldisplay.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int binaryToDecimal(const char *binary) {
+    int decimal = 0;
+    int length = strlen(binary);
+    for (int i = 0; i < length; i++) {
+        if (binary[length - i - 1] == '1') {
+            decimal += pow(2, i);
+        }
+    }
+    return decimal;
+}
+
+int main() {
+    char binaryNumber[65];  
+
+    printf("Enter a binary number: ");
+    scanf("%s", binaryNumber);
+
+    int decimalNumber = binaryToDecimal(binaryNumber);
+
+    printf("The decimal representation of %s is %d\n", binaryNumber, decimalNumber);
+
+    return 0;
+}
